1d4f28d1a2226e2e4fe2543492888c8e0eab829e
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2025-12-31 08:27:03 +0545

    Foundation of react

---
+++ App.js
@@ -0,0 +1,26 @@
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", {}, "I am h1 heading"),
+      React.createElement("h2", {}, "I am h2 heading"),
+    ]),
+    ,
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am h1 heading"),
+      React.createElement("h2", {}, "I am h2 heading"),
+    ]),
+  ]
+);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// );
+console.log(parent); // object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading); //convert heading object into h1 tag and put it in a DOM
+root.render(parent); //convert heading object into h1 tag and put it in a DOM
