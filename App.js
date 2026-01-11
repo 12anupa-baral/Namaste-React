5c130aae7948ddbef1d8a5485c3b000ffeed1a06
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2026-01-11 19:17:05 +0545

    understanding foundations

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; 
+import ReactDOM from "react-dom/client";
+
+// React.createElement=>ReactElement-JS object => html element
 const parent = React.createElement(
   "div",
   {
@@ -17,6 +19,43 @@ const parent = React.createElement(
     ]),
   ]
 );
+
+// react element
+// JSX =>babel trsnspiles it to React.createElement=>ReactElement-JS object =>html element
+const JSXHeading = <h1 id="heading">Hello from jsx</h1>;
+const heading = () => {
+  <h1 id="heading">Hello from jsx</h1>;
+};
+
+// react component
+
+const Title = () => {
+  return <h1 id="heading">Functional component</h1>;
+};
+
+const Title1 = function () {
+  return (
+   <h1 id="heading">Functional1 component</h1>
+)
+};
+
+
+// component composition
+const HeadingComponent = () => {
+
+  return <>
+    {JSXHeading}
+    <Title />
+    {/* {Title()}
+    <Title></Title> */}
+    {/* same as <Title /> */}
+    <Title1/>
+  <h1 id="heading">Hello from functional component</h1>
+  </>
+};
+
+console.log(JSXHeading);
 console.log(parent); // object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); //convert heading object into h1 tag and put it in a DOM
+root.render(JSXHeading); //convert heading object into h1 tag and put it in a DOM
+root.render(<HeadingComponent />); //convert heading object into h1 tag and put it in a DOM
